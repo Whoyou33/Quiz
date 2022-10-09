30ce9fcadeb10cbfd94c4268362cf39153ddf7b9
Robert Kruszniewski (105894) <75069@student.pb.edu.pl>
2022-10-09 23:10:45 +0200

    Quiz

---
+++ app/src/main/java/com/example/quiz2/Question.java
@@ -0,0 +1,17 @@
+package com.example.quiz2;
+
+public class Question {
+    private int questionID;
+    private boolean trueAnswer;
+
+    public Question(int questionID, boolean trueAnswer) {
+        this.questionID = questionID;
+        this.trueAnswer = trueAnswer;
+    }
+    public boolean isTrueAnswer(){
+        return trueAnswer;
+    }
+    public int getQuestionId(){
+        return questionID;
+    }
+}
